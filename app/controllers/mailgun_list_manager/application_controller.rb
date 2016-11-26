b32cf116fc7b533bbbe321d10c5b0be2f1068517
Noémien Kocher <nkcr.je@gmail.com>
2016-11-26 14:06:33 +0100

    controller name adeia

--- app/controllers/mailgun_list_manager/application_controller.rb
+++ app/controllers/mailgun_list_manager/application_controller.rb
@@ -1,6 +1,6 @@
 module MailgunListManager
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    load_and_authorize
+    load_and_authorize(controller: "mailgun_list_manager")
   end
 end
--- lib/mailgun_list_manager/version.rb
+++ lib/mailgun_list_manager/version.rb
@@ -1,3 +1,3 @@
 module MailgunListManager
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
